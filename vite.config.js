98a9bebc2c5c284f754ffbf1556af0cdc9e97955
hanzilarafiq78 <hanzilarafiq7@gmail.com>
2026-08-31 03:14:04 +0500

    commit all

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // or vue, etc. depending on your setup
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Cars-Website/',
+  plugins: [react()],
 })
-  assetsInclude: ['**/*.glb', '**/*.gltf', '**/*.hdr', '**/*.mp4', '**/*.jpeg'],
-  server: {
-    watch: {
-      // Ignore large video files to prevent EBUSY on Windows
-      ignored: ['**/public/videos/**'],
-    },
-  },
-  build: {
-    target: 'esnext',
-    minify: 'esbuild',
-  },
-});
