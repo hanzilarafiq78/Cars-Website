1077010800d279083bbc7ff8ae6c6f1d0aee76ca
Hanzila Rafiq <hanzilarafiq7@gmail.com>
2026-08-30 16:29:36 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react' // or vue, etc. depending on your setup
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Cars-Website/',
   plugins: [react()],
+  base: '/Cars-Website/',
+})
   assetsInclude: ['**/*.glb', '**/*.gltf', '**/*.hdr', '**/*.mp4', '**/*.jpeg'],
   server: {
     watch: {
